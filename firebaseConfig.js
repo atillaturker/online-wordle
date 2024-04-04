d9f9054960cd1b3e73fadb201dbdba6395c77dfd
Oğuzhan Türker <oguzturker8@gmail.com>
2024-04-04 16:11:32 +0300

    add firebase integration

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD2JK-Z02muxJf4Gg2D09K5h1cgH1_YlWY",
+  authDomain: "online-wordle-8c404.firebaseapp.com",
+  projectId: "online-wordle-8c404",
+  storageBucket: "online-wordle-8c404.appspot.com",
+  messagingSenderId: "257440840716",
+  appId: "1:257440840716:web:b7b89eb5d388d90e754229",
+};
+export const app = initializeApp(firebaseConfig);
+
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
