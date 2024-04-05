18309bc1fabdbc517ca84f269a6d482fd490c32f
atillaturker <atillaturker2@gmail.com>
2024-04-06 00:22:05 +0300

    add bottom navigation

--- src/navigation/auth.jsx
+++ src/navigation/auth.jsx
@@ -1,17 +1,20 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as React from "react";
 import { Homepage } from "../screens";
 import { SCREENS } from "./config";
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export const Auth = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name={SCREENS.homepage} component={Homepage} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name={SCREENS.homepage} component={Homepage} />
+        <Tab.Screen name="2.ekran" component={Homepage} />
+        <Tab.Screen name="3.ekran" component={Homepage} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
