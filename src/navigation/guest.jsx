6a57076ddcd23c1345cae00da7368251e345fa49
atillaturker <atillaturker2@gmail.com>
2024-04-08 18:57:13 +0300

    realtime-db-rooms

--- src/navigation/guest.jsx
+++ src/navigation/guest.jsx
@@ -1,15 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as React from "react";
+import { useMMKVStorage } from "react-native-mmkv-storage";
 
+import { storage } from "../../App";
 import { RegisterScreen, SignInScreen, WelcomeScreen } from "../screens";
 import { SCREENS } from "./config";
+
 const Stack = createNativeStackNavigator();
 
 export const Guest = () => {
+  const [user, _] = useMMKVStorage("user", storage, {
+    username: "",
+    password: "",
+  });
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        initialRouteName={user.username ? SCREENS.signin : SCREENS.welcome}
+      >
         <Stack.Screen name={SCREENS.welcome} component={WelcomeScreen} />
         <Stack.Screen name={SCREENS.register} component={RegisterScreen} />
         <Stack.Screen name={SCREENS.signin} component={SignInScreen} />
--- src/screens/homepage.jsx
+++ src/screens/homepage.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import { onValue, ref } from "firebase/database";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { DataTable } from "react-native-paper";
+import { FIREBASE_DATABASE } from "../../firebaseConfig";
 
 export const Homepage = () => {
+  const [rooms, setRooms] = useState([]);
+  useEffect(() => {
+    const starCountRef = ref(FIREBASE_DATABASE);
+    onValue(starCountRef, (snapshot) => {
+      const { rooms } = snapshot.val();
+      setRooms(rooms);
+    });
+  }, []);
   return (
     <View>
       <Text>Hello from homepage</Text>
+      <DataTable>
+        <DataTable.Header>
+          <DataTable.Title>NO</DataTable.Title>
+          <DataTable.Title>Ad</DataTable.Title>
+          <DataTable.Title>Kullanici Sayisi</DataTable.Title>
+        </DataTable.Header>
+
+        {rooms.map((room) => (
+          <DataTable.Row key={room.id}>
+            <DataTable.Cell>{room.id}</DataTable.Cell>
+            <DataTable.Cell>{room.name}</DataTable.Cell>
+            <DataTable.Cell>{`${room.users}/2`}</DataTable.Cell>
+          </DataTable.Row>
+        ))}
+      </DataTable>
     </View>
   );
 };
